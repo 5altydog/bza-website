be41d04ded440cac4590bf3104b74e69b41980b6
5altydog <tedwillis@gmail.com>
2025-08-12 22:37:41 -0700

    restored vite.config.ts changes

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // no custom base needed for Vercel
-})
-;
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
